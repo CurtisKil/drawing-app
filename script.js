f72387027fe24367c1d7c463279234632bb09cd0
Curtis <curtis@curtiss-mbp.home>
2021-04-07 12:58:35 -0700

    Mouse Events & Drawing

--- script.js
+++ script.js
@@ -2,9 +2,44 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let size = 20;
+// Is the mouse pressed (mousedown event)
+let isPressed = false;
 let color = "black";
 let x;
 let y;
+
+// User presses mouse (mousedown)
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+
+  // Positioning of where the mouse is
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+// User releases mouse press (mouseup)
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+
+  // Reset positioning
+  x = undefined;
+  y = undefined;
+});
+
+// User moves mouse (mousemove)
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    // Set x2, y2 to current position
+    x = x2;
+    y = y2;
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -20,6 +55,7 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineTo(x2, y2);
   //   Apply color. Stroke style is like a border/line
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  //   Make circle same size as line
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
